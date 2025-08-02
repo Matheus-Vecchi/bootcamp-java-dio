9855edc53914a2cfa25ef8c321197c1efe744eb5
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-08-02 20:29:02 -0300

    feat: finaliza exercício ContaTerminal com entrada e exibição de dados

--- 01-conta-bancaria/ContaTerminal.java
+++ 01-conta-bancaria/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o seu saldo: ");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("NOME: " + nome + "\nCONTA: " + conta + "\nAGÊNCIA: " + agencia + "\nSALDO: R$" + saldo);
+
+        scanner.close();
     }
 }
